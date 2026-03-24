75233562963e80af23bf790a6202999168eafe53
MRADUL KUMAR KATIYAR <87556435+Karelaking@users.noreply.github.com>
2026-03-24 23:11:21 +0530

    fix: correct typo in footer text

--- app/layout.js
+++ app/layout.js
@@ -38,7 +38,7 @@ export default function RootLayout({ children }) {
 
             <footer className="bg-muted/50 py-12">
               <div className="container mx-auto px-4 text-center text-gray-200">
-                <p>Made with 💗 by Pluse tech</p>
+                <p>Made with 💗 by Pulse tech</p>
               </div>
             </footer>
           </ThemeProvider>
